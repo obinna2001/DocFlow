19a325def450b43a322da5d8c886f8ea3ee80d4a
OkeyObinna <obinna.okey@indicina.co>
2026-07-16 09:18:00 +0100

    feat(test): add test fixture for upload_document endpoint

--- test/test_upload.py
+++ test/test_upload.py
@@ -1,9 +1,16 @@
 from pathlib import Path 
+import pytest
+
 
 test_file = Path("/data/test_documentTrier.pdf")
 
 
+@pytest.mark.anyio
+async def test_upload_document(async_client):
+    """test_upload_document tests the upload_document POST endpoint in DocFlow"""
+    request = {
 
-async def test_upload_file(async_client):
-    """"""
+    }
+    response = await async_client.post("/document/upload", request)
+    assert response.status_code == 200
 
